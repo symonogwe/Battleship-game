3848512af451d1d5e55e4d905aefe93f91fe935a
Symon Ogwe <symonogwe@gmail.com>
2023-11-13 16:40:29 +0300

    Add test to check illegal horizontal moves

--- src/GameBoard.test.js
+++ src/GameBoard.test.js
@@ -44,4 +44,26 @@ test("GameBoards should be able to place ships at specific coordinates verticall
   );
 });
 
-// test("Can't make an illegal vertical move");
+test("Ships can't make an illegal vertical move", () => {
+  gameBoard1.placeShip = jest.fn((coordinate, length) => {
+    const ship1 = new Ship(length);
+    const start = coordinate[0];
+    const end = coordinate[1];
+    const arr = gameBoard1.gameBoard[start];
+    const targetStartIndex = arr.slice(end);
+
+    if (length > targetStartIndex.length) {
+      return "Invalid move";
+    }
+
+    for (let i = end; i <= length; i++) {
+      arr[i] = ship1;
+    }
+
+    gameBoard1.gameBoard[start] = arr;
+    return gameBoard1.gameBoard;
+  });
+
+  gameBoard1.placeShip([3, 6], 5);
+  expect(gameBoard1.placeShip.mock.results[0].value).toBe("Invalid move");
+});
