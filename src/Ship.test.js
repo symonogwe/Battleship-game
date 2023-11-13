d877c87cfc170e0c8e1056b4fee1630711db43df
Symon Ogwe <symonogwe@gmail.com>
2023-11-13 07:43:17 +0300

    Create Ship.test file & add tests for Ship class

--- src/Ship.test.js
+++ src/Ship.test.js
@@ -1,28 +1,54 @@
-// ship object
+import Ship from "./Ship";
+
 const ship1 = new Ship(5);
-// mock ship functions
-ship1.prototype.hit = jest.fn(() => {
-  this.hits++;
-  return this.hits;
+// tests if ship has required properties
+test("Tests if our ship has exact properties", () => {
+  expect(ship1).toHaveProperty("length", 5);
+  expect(ship1).toHaveProperty("hits", 0);
+  expect(ship1).toHaveProperty("sunk", false);
 });
 
-ship1.prototype.isSunk = jest.fn(() => {
-  if (this.length === this.hits) {
-    this.sunk = true;
-    return "ship is sunk";
-  }
-  return "ship is not sunk";
+// tests if ship has required methods
+ship1.hit = jest.fn();
+ship1.hit();
+
+ship1.isSunk = jest.fn();
+ship1.isSunk();
+
+test("Tests if our ship has exact methods", () => {
+  expect(ship1.hit.mock.calls).toHaveLength(1);
+  expect(ship1.isSunk.mock.calls).toHaveLength(1);
 });
 
-test("Tests the ship object has exact properties", () => {
-  expect(ship1).toEqual({
-    length: 5,
-    hits: 0,
-    sunk: false,
+// tests whether hit method increases hit property
+test("tests whether hit method increases hit property", () => {
+  const ship2 = new Ship(3);
+  ship2.hit = jest.fn(() => {
+    ship2.hits++;
+    return ship2.hits;
   });
+  ship2.hit();
+  ship2.hit();
+  ship2.hit();
+
+  expect(ship2.hit.mock.results[0].value).toBe(1);
+  expect(ship2.hit.mock.results[1].value).toBe(2);
+  expect(ship2.hit.mock.results[2].value).toBe(3);
 });
 
-test("Tests the ship object has exact methods", () => {
-  expect(ship1.hit.mock.results[0].value).toBe(1);
-  expect(ship1.isSunk.mock.results[0].value).toBe("ship is not sunk");
+// tests whether isSunk functionality works
+test("tests whether isSunk functionality works", () => {
+  const ship3 = new Ship(2);
+  ship3.hits = 2;
+
+  ship3.isSunk = jest.fn(() => {
+    if (ship3.hits === ship3.length) {
+      ship3.sunk = true;
+      return ship3.isSunk;
+    }
+    return ship3.sunk;
+  });
+  ship3.isSunk();
+
+  expect(ship3.isSunk.mock.results[0].value).toBeTruthy();
 });
