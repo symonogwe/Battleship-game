6c8ed263f70690ec2bab9fe346f1fa706d62b412
Symon Ogwe <symonogwe@gmail.com>
2023-11-13 07:44:33 +0300

    Create Ship.js & Create Shpp class

---
+++ src/Ship.js
@@ -0,0 +1,18 @@
+// Ship class
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+    this.sunk = false;
+  }
+  hit() {
+    this.hits++;
+  }
+  isSunk() {
+    if (this.length === this.hits) {
+      this.sunk = true;
+    }
+  }
+}
+
+export default Ship;
