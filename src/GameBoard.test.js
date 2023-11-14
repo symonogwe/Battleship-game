496cdee36bc10eed9838cd836849ec9991e4b177
Symon Ogwe <symonogwe@gmail.com>
2023-11-14 07:48:30 +0300

    Tests placing ship on another ship horizontally

--- src/GameBoard.test.js
+++ src/GameBoard.test.js
@@ -77,42 +77,56 @@ test("Ships can't make an illegal horizontal move", () => {
 });
 
 // // GameBoard shouldn't place ship on another ship
-// const gameBoard3 = new GameBoard();
-// test("GameBoard shouldn't place ship on another ship", () => {
-//   const target1 = new GameBoard();
-//   target1.gameBoard[2][3] = new Ship(3);
-//   target1.gameBoard[2][4] = new Ship(3);
-//   target1.gameBoard[2][5] = new Ship(3);
+const gameBoard3 = new GameBoard();
+test("GameBoard shouldn't place ship on another ship", () => {
+  const target1 = new GameBoard();
+  target1.gameBoard[2][3] = new Ship(3);
+  target1.gameBoard[2][4] = new Ship(3);
+  target1.gameBoard[2][5] = new Ship(3);
 
-//   gameBoard3.placeShipHorizontally = jest.fn((coordinate, length) => {
-//     const ship1 = new Ship(length);
-//     const start = coordinate[0];
-//     const end = coordinate[1];
-//     const arr = gameBoard3.gameBoard[start];
-//     const targetStartIndex = arr.slice(end);
+  gameBoard3.placeShipHorizontally = jest.fn((coordinate, length) => {
+    const ship1 = new Ship(length);
+    const start = coordinate[0];
+    let end = coordinate[1];
+    const arr = gameBoard3.gameBoard[start];
+    const targetStartIndex = arr.slice(end);
+    let times = 0;
+    let checkExistenceTimes = 0;
+    let checkEnd = coordinate[1];
 
-//     if (length > targetStartIndex.length) {
-//       return "Invalid move";
-//     }
+    if (length > targetStartIndex.length) {
+      return "Invalid move";
+    }
 
-//     for (let i = end; i <= length; i++) {
-//       if (arr[i] !== 0) return "Can't place ship on another ship";
-//     }
+    while (checkExistenceTimes < length) {
+      if (arr[checkEnd] !== 0) return "Can't place ship on another ship";
+      checkExistenceTimes++;
+      checkEnd++;
+    }
 
-//     for (let i = end; i <= length; i++) {
-//       arr[i] = ship1;
-//     }
+    while (times < length) {
+      arr[end] = ship1;
+      times++;
+      end++;
+    }
 
-//     // gameBoard3.gameBoard[start] = arr;
-//     gameBoard3.gameBoard[start] = arr.map((i) => i);
-//     return gameBoard3.gameBoard;
-//   });
-//   gameBoard3.placeShipHorizontally([2, 3], 3);
-//   gameBoard3.placeShipHorizontally([2, 1], 3);
-//   expect(gameBoard3.placeShipHorizontally.mock.results[0].value).toEqual(
-//     target1.gameBoard
-//   );
-// });
+    gameBoard3.gameBoard[start] = arr;
+    return gameBoard3.gameBoard;
+  });
+  gameBoard3.placeShipHorizontally([2, 3], 3);
+  gameBoard3.placeShipHorizontally([2, 1], 3);
+  gameBoard3.placeShipHorizontally([2, 5], 2);
+
+  expect(gameBoard3.placeShipHorizontally.mock.results[0].value).toEqual(
+    target1.gameBoard
+  );
+  expect(gameBoard3.placeShipHorizontally.mock.results[1].value).toBe(
+    "Can't place ship on another ship"
+  );
+  expect(gameBoard3.placeShipHorizontally.mock.results[2].value).toBe(
+    "Can't place ship on another ship"
+  );
+});
 
 // GameBoards should be able to place ships at specific coordinates vertically
 const gameBoard2 = new GameBoard();
