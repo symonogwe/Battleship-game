809ced65cd6db11d94faf30a7a424681094958c1
Symon Ogwe <symonogwe@gmail.com>
2023-11-15 11:58:08 +0300

    Add playRound method to Player class

---
+++ src/Player.js
@@ -0,0 +1,15 @@
+import GameBoard from "./GameBoard";
+
+class Player {
+  constructor(name, enemy) {
+    this.name = name;
+    this.enemy = null;
+    this.board = new GameBoard();
+  }
+
+  playRound(coordinates) {
+    return this.enemy.board.receiveAttack(coordinates);
+  }
+}
+
+export default Player;
