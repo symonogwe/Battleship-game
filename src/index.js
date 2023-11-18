39648337e8093c45680902f021160349f22d2609
Symon Ogwe <symonogwe@gmail.com>
2023-11-18 10:00:57 +0300

    import all GameLoop components to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import * as ShipComponents from "./Ship";
 import * as GameBoardComponents from "./GameBoard";
 import * as PlayerComponents from "./Player";
+import * as GameLoopComponents from "./GameLoop";
