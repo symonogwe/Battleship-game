96b07e9a74f14f5ce65273cae1ebf89d50c86f65
Symon Ogwe <symonogwe@gmail.com>
2023-11-21 12:44:34 +0300

    Import all screenUpdater components to index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import * as ShipComponents from "./Ship";
 import * as GameBoardComponents from "./GameBoard";
 import * as PlayerComponents from "./Player";
 import * as GameLoopComponents from "./GameLoop";
+import * as ScreenUpdaterComponents from "./ScreenUpdater";
+
+import "./Styles/styles.css";
