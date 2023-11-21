7cba8fc3c9d8d32f644ad79540039778595f8df2
Symon Ogwe <symonogwe@gmail.com>
2023-11-21 12:41:02 +0300

    Delete post-css in webpack loaders & ran build

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ const config = {
       },
       {
         test: /\.css$/i,
-        use: [stylesHandler, "css-loader", "postcss-loader"],
+        use: [stylesHandler, "css-loader"],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
