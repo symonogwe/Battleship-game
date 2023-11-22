4e0d3eee19850577a16ce4e2606a8ebef349fa40
Symon Ogwe <symonogwe@gmail.com>
2023-11-22 07:35:14 +0300

    Add renderEmptyPlayer1Board utility function
    - Displays board so player can place ships

--- src/ScreenUpdater.js
+++ src/ScreenUpdater.js
@@ -17,8 +17,15 @@ class ScreenUpdater {
     this.mainGame.playerArray[0] = this.mainGame.player1;
     this.mainGame.currentPlayer = this.mainGame.playerArray[0];
 
-    // clear form input
+    // clear form input & hide it
     clearInput(form);
+    hideElement(form);
+    this.placePlayer1Ships();
+  }
+  placePlayer1Ships() {
+    console.log(this.mainGame.player1);
+    const player1Board = this.mainGame.player1.board.gameBoard;
+    renderEmptyPlayer1Board(player1Board);
   }
 }
 
@@ -35,4 +42,31 @@ player1Btn.addEventListener("click", () => {
 // UTILITY FUNCTIONS
 function clearInput(form) {
   form.reset();
+}
+
+function hideElement(element) {
+  element.style.display = "none";
+}
+
+function renderEmptyPlayer1Board(board) {
+  const startingBoard = document.querySelector(".player-1-empty-board");
+  for (let i = 0; i < board.length; i++) {
+    const row = board[i];
+
+    for (let j = 0; j < row.length; j++) {
+      const cell = row[j];
+      const cellDiv = document.createElement("div");
+      cellDiv.classList.add("cell-div");
+      // const cellP = document.createElement("p");
+      // cellP.classList.add("cell-p");
+      // cellP.textContent = `${i}, ${j}`;
+      // cellDiv.append(cellP);
+      // cellDiv.textContent = `${i}, ${j}`;
+
+      cellDiv.dataset.x = i;
+      cellDiv.dataset.y = j;
+
+      startingBoard.appendChild(cellDiv);
+    }
+  }
 }
