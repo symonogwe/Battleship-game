f6272e41e2f2d01e605046905bd1dc04dabfd69a
Symon Ogwe <symonogwe@gmail.com>
2023-11-23 10:37:57 +0300

    Import ScreenController components to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import * as ShipComponents from "./Ship";
 import * as GameBoardComponents from "./GameBoard";
 import * as PlayerComponents from "./Player";
 import * as GameLoopComponents from "./GameLoop";
-import * as ScreenUpdaterComponents from "./ScreenUpdater";
+// import * as ScreenUpdaterComponents from "./ScreenUpdater";
+import * as ScreenControllerComponents from "./ScreenController";
 
 import "./Styles/styles.css";
