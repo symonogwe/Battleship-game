a6f17095bd5da52cf8228fe78619698fa1d5e624
Symon Ogwe <symonogwe@gmail.com>
2023-11-25 12:24:50 +0300

    Import all of player1DomModule Components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as ShipComponents from "./Ship";
 import * as GameBoardComponents from "./GameBoard";
 import * as PlayerComponents from "./Player";
 import * as GameLoopComponents from "./GameLoop";
-// import * as ScreenUpdaterComponents from "./ScreenUpdater";
 import * as ScreenControllerComponents from "./ScreenController";
+import * as Player1DomModuleComponents from "./Player1DomModule";
 
 import "./Styles/styles.css";
