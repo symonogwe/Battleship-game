e875b1c456f37ddbe82f45d053c2b72bb35a0ab3
Symon Ogwe <symonogwe@gmail.com>
2023-11-27 17:49:14 +0300

    Add functionality to attack opposing player's ship

--- src/PlayGame.js
+++ src/PlayGame.js
@@ -1,10 +1,13 @@
 import screenController from "./ScreenController";
 import skeletonImg from "./Assets/skeleton.svg";
 import shipImg from "./Assets/ship.svg";
+import { clearText, deleteAllChildren } from "./Player1DomModule";
 
 // RENDER PLAYER GAME-BOARDS FUNCTIONALITY
 function renderPlayer1GameBoard(targetDiv) {
   targetDiv.style.cursor = "not-allowed";
+
+  deleteAllChildren(targetDiv);
 
   const player1Board = screenController.mainGame.player1.board.gameBoard;
 
@@ -36,12 +39,18 @@ function renderPlayer1GameBoard(targetDiv) {
         gameCell.appendChild(skeleton);
       }
 
+      if (cell === 1) {
+        gameCell.style.backgroundColor = "orange";
+      }
+
       targetDiv.appendChild(gameCell);
     }
   }
 }
 
 function renderPlayer2GameBoard(targetDiv) {
+  deleteAllChildren(targetDiv);
+
   const player2Board = screenController.mainGame.player2.board.gameBoard;
 
   for (let i = 0; i < player2Board.length; i++) {
@@ -64,6 +73,75 @@ function renderPlayer2GameBoard(targetDiv) {
         skeleton.classList.add("skeleton-img");
 
         gameCell.appendChild(skeleton);
+      }
+
+      if (cell === 1) {
+        gameCell.style.backgroundColor = "orange";
+      }
+
+      gameCell.addEventListener("click", () => {
+        attackComputer(gameCell);
+      });
+
+      targetDiv.appendChild(gameCell);
+    }
+  }
+}
+
+function attackComputer(targetDiv) {
+  const coordinates = [+targetDiv.dataset.x, +targetDiv.dataset.y];
+
+  const message = screenController.mainGame.currentPlayerTurn(coordinates);
+  const gamePlayResults = document.querySelector(".game-play-results");
+  clearText(gamePlayResults);
+
+  gamePlayResults.textContent = message;
+
+  player2BoardResults();
+
+  setTimeout(attackPlayer1, 1000);
+}
+
+function attackPlayer1() {
+  const gamePlayResults = document.querySelector(".game-play-results");
+  clearText(gamePlayResults);
+
+  const message = screenController.mainGame.currentPlayerTurn();
+  gamePlayResults.textContent = message;
+
+  screenController.renderBothGameBoards();
+}
+
+function player2BoardResults() {
+  const targetDiv = document.querySelector(".player-2-board");
+  deleteAllChildren(targetDiv);
+
+  const player2Board = screenController.mainGame.player2.board.gameBoard;
+
+  for (let i = 0; i < player2Board.length; i++) {
+    const row = player2Board[i];
+
+    for (let j = 0; j < row.length; j++) {
+      const cell = row[j];
+
+      const gameCell = document.createElement("div");
+      gameCell.classList.add("game-cell");
+
+      gameCell.style.cursor = "none";
+
+      gameCell.dataset.x = i;
+      gameCell.dataset.y = j;
+
+      if (cell === "hit") {
+        const skeleton = document.createElement("img");
+        skeleton.src = skeletonImg;
+        skeleton.classList.add("skeleton-img");
+
+        gameCell.appendChild(skeleton);
+      }
+
+      if (cell === 1) {
+        gameCell.style.backgroundColor = "orange";
       }
 
       targetDiv.appendChild(gameCell);
